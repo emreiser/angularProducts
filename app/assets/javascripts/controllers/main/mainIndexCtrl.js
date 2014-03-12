0a971fa00a9ea68f66e3d15799786a945bcca0bb
emreiser <emilie.reiser@gmail.com>
2014-03-11 21:27:41 -0400

    Initial commit. Setup of Products index and show with Angular.

---
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -0,0 +1,17 @@
+var IndexCtrl = function($scope, $location, $http, productData){
+
+	$scope.data = {};
+
+	productData.loadProducts(function(data){
+		$scope.data.products = data.products;
+	});
+
+  $scope.viewPost = function(productId){
+    $location.url('/products/' + productId);
+  };
+
+  $scope.navNewProduct = function(){
+    $location.url('/product/new');
+  };
+
+};
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularProducts::Application.config.secret_key_base = '4635ebdb69c0bc111f8d624b2c7c1d0efa966aaa08b7c95306d57f6308b30d3cb6e4b12d2ab3df6ffdbbdcba5f5bd9586aabd445ed3f7cb73142b04c7dec3c14'
